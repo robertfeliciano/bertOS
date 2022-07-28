36315b312ab38278c3ec40ac71f3c12d9f015590
robertfeliciano <robertfeliciano314@gmail.com>
2022-07-28 17:21:36 -0400

    update header files

--- include/entry.h
+++ include/entry.h
@@ -1,7 +1,7 @@
 #ifndef _ENTRY_H
 #define _ENTRY_H
 
-#define S_FRAME_SIZE            256
+#define S_FRAME_SIZE            272
 
 #define SYNC_INVALID_EL1t       0
 #define IRQ_INVALID_EL1t        1
@@ -22,5 +22,11 @@
 #define IRQ_INVALID_EL0_32      13 
 #define FIQ_INVALID_EL0_32      14 
 #define ERROR_INVALID_EL0_32    15 
+
+#ifndef __ASSEMBLER__
+
+void ret_from_fork();
+
+#endif
 
 #endif
--- include/mm.h
+++ include/mm.h
@@ -1,6 +1,8 @@
 #ifndef    _MM_H
 #define    _MM_H
 
+#include <pi/base.h>
+
 #define PAGE_SHIFT          12
 #define TABLE_SHIFT         9
 #define SECTION_SHIFT       (PAGE_SHIFT + TABLE_SHIFT)
@@ -9,10 +11,16 @@
 #define SECTION_SIZE        (1 << SECTION_SHIFT)    
 
 #define LOW_MEMORY          (2 * SECTION_SIZE)
+#define HIGH_MEMORY         PBASE
+
+#define PAGING_MEMORY       (HIGH_MEMORY - LOW_MEMORY)
+#define PAGING_PAGES        (PAGING_MEMORY/PAGE_SIZE)
 
 #ifndef __ASSEMBLER__
 
 void memzero(unsigned long src, unsigned long n);
+unsigned long get_free_page();
+void free_page(unsigned long p);
 
 #endif
 
